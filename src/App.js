21ee49c273f58d3c4afd5d241bae93b16a4ca377
germanalmiron <german.almiron13@gmail.com>
2021-11-18 21:33:50 -0300

    db local

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CrudApp from "./components/CrudApp";
+
+function App() {
+  return (
+    <div>
+      <h1>Laboratorio de Computación II - TUP Villa Elisa</h1>
+      <CrudApp/>
+    </div>
+  );
+}
+
+export default App;
